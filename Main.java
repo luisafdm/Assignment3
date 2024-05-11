963ef9b887fb6b71a8f5c12e13904326fe9807b7
luisafdm <125517336+luisafdm@users.noreply.github.com>
2024-05-10 23:10:58 -0500

    trabajito

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
 public class Main {
     public static void main (String[] args){
         fecha();
+        hora();
     }
     static void fecha(){
         System.out.println("10 de mayo");
+    }
+    static void hora(){
+        System.out.println("Son las 8:00");
     }
 }
